7ae725c0356ee8be957734057de8223f4b4bab18
Andrew Luna <andrewluna94@hotmail.com>
2016-11-22 21:56:21 -0800

    finished stories

--- Project2/productview.cpp
+++ Project2/productview.cpp
@@ -16,10 +16,23 @@ productView::~productView()
 
 void productView::initImages() {
 
+    ui->textEdit->setReadOnly(1);
+    ui->textEdit_2->setReadOnly(1);
+    ui->textEdit_3->setReadOnly(1);
+    ui->textEdit_4->setReadOnly(1);
+    ui->textEdit_5->setReadOnly(1);
+    ui->textEdit_6->setReadOnly(1);
+
+
     QLabel *label1 = ui->label;
     QImage *image1 = new QImage("../Resources/robot1.jpg");
     QImage test1 = image1->scaled(label1->width(), label1->height(), Qt::KeepAspectRatio);
     label1->setPixmap(QPixmap::fromImage(test1));
+
+    QLabel *label2 = ui->label_2;
+    QImage *image2 = new QImage("../Resources/robot2.jpg");
+    QImage test2 = image2->scaled(label2->width(), label2->height(), Qt::KeepAspectRatio);
+    label2->setPixmap(QPixmap::fromImage(test2));
 
     QLabel *label3 = ui->label_3;
     QImage *image3 = new QImage("../Resources/robot3.jpg");
