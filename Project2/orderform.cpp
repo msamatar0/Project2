538acf47e527776b0f143b57072eff8d5655ae00
jakeypakey <jpollard5@saddleback.edu>
2016-12-03 17:15:57 -0800

    formatting

--- Project2/orderform.cpp
+++ Project2/orderform.cpp
@@ -24,13 +24,13 @@ void orderform::on_pushButton_2_clicked()
 {
     double total;
 
-    total = (ui->spin1->value()*1999.99) + (ui->spin2->value()*9999.99) + (ui->spin3->value()*40000);
+    total = (double)(ui->spin1->value()*1999.99) + (double)(ui->spin2->value()*9999.99) + (double)(ui->spin3->value()*40000);
     if(total==0){
         QMessageBox::warning(this,"Error","You have not selected any items");
     }
     else{
         QMessageBox::information(this,"Order Placed",
-                                 "Your total is: $" + QString::number(total) + "\nYour order, as well as your bill will be sent"
+                                 "Your total is: $" + QString::number(total,'f',2) + "\nYour order, as well as your bill will be sent"
                                                               " to:\n\n" + record.getAddress(record.getUserIndex()));
         this->parentWidget()->show();
         this->close();
